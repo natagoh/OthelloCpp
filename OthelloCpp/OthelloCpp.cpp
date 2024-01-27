ab4536b167d95f72ac51e99f84676329b33b50a3
natagoh <gohnatasha98@gmail.com>
2024-01-26 22:14:19 -0800

    Add project files.

---
+++ OthelloCpp/OthelloCpp.cpp
@@ -0,0 +1,12 @@
+﻿// OthelloCpp.cpp : Defines the entry point for the application.
+//
+
+#include "OthelloCpp.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}

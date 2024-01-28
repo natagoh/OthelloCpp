088bfd7a47fa4b0e002eedd215d19443915520c8
natagoh <gohnatasha98@gmail.com>
2024-01-28 02:05:34 -0800

    got SFML to draw a circle

--- main.cpp
+++ main.cpp
@@ -1,21 +1,39 @@
 #include <SFML/Graphics.hpp>
 
+
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+    // create the window
+    auto window = sf::RenderWindow{ { 1080u, 1080u }, "CMake SFML Project" };
+    window.setFramerateLimit(60);
 
+    // run the program as long as the window is open
     while (window.isOpen())
     {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
         {
+            // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
-            {
                 window.close();
-            }
         }
 
-        window.clear();
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+        sf::CircleShape shape(50.f);
+
+        // set the shape color to green
+        shape.setFillColor(sf::Color(100, 250, 50));
+
+        window.draw(shape);
+
+        // draw everything here...
+        // window.draw(...);
+
+        // end the current frame
         window.display();
     }
+
+    return 0;
 }

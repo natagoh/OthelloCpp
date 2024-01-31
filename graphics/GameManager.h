6f45f9321a1b871cdf738644a0a6647ce9b79a79
natagoh <gohnatasha98@gmail.com>
2024-01-30 23:35:14 -0800

    player interface not working

--- graphics/GameManager.h
+++ graphics/GameManager.h
@@ -2,25 +2,28 @@
 
 #include "../othello/OthelloBoard.h"
 #include "GameView.h"
+#include "../players/PlayerInterface.h"
 
 #include <optional>
 #include <SFML/Graphics.hpp>
 
+/*
 struct Player {
 	Piece color;
 
 	Player(Piece color) : color(color) {}
 };
-
+*/
 
 enum class GameStepOutcome {
 	Action,		// player finished their turn, supplied an action
 	Pending,	// still waiting for player to finish their turn and supply an action
 };
+
+
 class GameManager {
 private:
 	// black goes first
-	Player _players[2] = {Player(Piece::Black), Player(Piece::White)};
 	OthelloBoard _othello;
 	sf::RenderWindow _window;
 	GameView _gameView;
@@ -35,7 +38,7 @@ private:
 	const std::optional<sf::Vector2i> getMouseClickPos();
 
 	GameStepOutcome gameStep(
-		const Player& player, 
+		std::shared_ptr<PlayerInterface> player,
 		bool isHuman = false, 
 		const std::optional<sf::Vector2i> &mousePos = std::nullopt
 	);
